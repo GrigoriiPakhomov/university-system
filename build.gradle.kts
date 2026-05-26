4a11212980a4e748bba21760337ffff010221dd5
GrigoriiPakhomov <pakhomov-86@list.ru>
2026-05-26 23:35:46 +0800

    Добавил lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,27 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    // JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // AssertJ
+    testImplementation("org.assertj:assertj-core:3.27.3")
 }
 
 tasks.test {
